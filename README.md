dfc6e193cab16d2ec58bbfc77a34f921f111c487
Nikhil <nikpat1988@gmail.com>
2023-08-08 10:42:00 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+This is test project
+
+## How to run project?
+
+1. Create and activate a virtual environment:
+
+    ```sh
+    $ python3 -m venv venv && source venv/bin/activate
+    ```
+
+1. Install the requirements:
+
+    ```sh
+    (venv)$ pip install -r requirements.txt
+    ```
+
+1. Run the app:
+
+    ```sh
+    (venv)$ python main.py
+    ```
+
+1. Test at [http://localhost:8081/docs](http://localhost:8081/docs)
---
+++ app/model.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class PostSchema(BaseModel):
+    id: int = Field(default=None)
+    title: str = Field(...)
+    content: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "FastAPI test post",
+                "content": "Test post for test assignement"
+            }
+        }
+
+
+class UserSchema(BaseModel):
+    fullname: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "fullname": "Amit Patel",
+                "email": "amitp2184@gmail.com",
+                "password": "asdasd"
+            }
+        }
+
+class UserLoginSchema(BaseModel):
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "amitp2184@gmail.com",
+                "password": "asdasd"
+            }
+        }
